10a03a6ccffa06df01c0a7f5a6fafe47cde24080
Indrahas <ihreddy22@gmail.com>
2024-11-10 17:02:37 +0000

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.List;
 
 public class Main {
   static HashMap<Character, TokenType> lexGrammar = new HashMap<>();
@@ -34,68 +35,109 @@ public class Main {
     // Uncomment this block to pass the first stage
 
      if (!fileContents.isEmpty()) {
+       List<Token> tokens = new java.util.ArrayList<>(List.of());
        grammarInit();
+
        boolean syntaxError = false;
        String token;
        TokenType tokenType;
        Character curChar;
-        int lineNo = 1;
+
+       int lineNo = 1;
+
        for(int i = 0; i<fileContents.length(); i++){
+
          curChar = fileContents.charAt(i);
+
          if(Character.isWhitespace(curChar) ||
            Character.isSpaceChar(curChar)){
            if(curChar.equals('\n')) lineNo++;
            continue;
          }
+
          token = String.valueOf(curChar);
+
          if(lexGrammar.containsKey(curChar))
          {
-           tokenType = lexGrammar.get(curChar);
-           if((i+1) < fileContents.length()){
-             if(fileContents.startsWith("!=", i)){
-               tokenType = TokenType.BANG_EQUAL;
-               token = "!=";
-               i++;
-             } else if (fileContents.startsWith("<=", i)){
-               tokenType = TokenType.LESS_EQUAL;
-               token = "<=";
-               i++;
-             }
-             else if (fileContents.startsWith(">=", i)){
-               tokenType = TokenType.GREATER_EQUAL;
-               token = ">=";
-               i++;
-             }
-             else if (fileContents.startsWith("==", i)){
-               tokenType = TokenType.EQUAL_EQUAL;
-               token = "==";
-               i++;
-             }
-             else if (fileContents.startsWith("//", i)){
-               if(fileContents.indexOf("\n", i)!=-1){
-                 i = fileContents.indexOf("\n", i);
-                 lineNo+=1;
-                 continue;
-               }
-               break;
-             }
-           }
-           System.out.println(tokenType + " " + token + " null");
-         }
 
+           tokenType = lexGrammar.get(curChar);
+
+           if (fileContents.startsWith("//", i)){
+             if(fileContents.indexOf("\n", i)!=-1){
+               i = fileContents.indexOf("\n", i);
+               lineNo+=1;
+               continue;
+             }
+             break;
+           }
+
+           Token longerLex = checkLongerLex(fileContents, i, lineNo);
+
+           if(longerLex!=null){
+             tokens.add(longerLex);
+             i++;
+           }
+           else if(curChar.equals('"')){
+
+             int strLiteralEndInd = fileContents.indexOf('"', i+1);
+             String strLiteral = fileContents.substring(i, strLiteralEndInd+1);
+             tokens.add(new Token(TokenType.STRING,
+                                  fileContents.substring(i, strLiteralEndInd+1),
+                                  fileContents.substring(i+1, strLiteralEndInd),
+                                  lineNo));
+             i = strLiteralEndInd;
+
+           }
+           else{
+             tokens.add(new Token(tokenType, token, null, lineNo));
+           }
+
+         }
          else{
+
            System.err.println("[line "+lineNo+"] Error: Unexpected character: "+token);
            syntaxError = true;
 
          }
-
        }
+
+       for(Token curToken: tokens){
+         System.out.println(curToken.toString());
+       }
+
        System.out.println("EOF  null");
        if(syntaxError) System.exit(65);
-     } else {
 
+     } else {
        System.out.println("EOF  null"); // Placeholder, remove this line when implementing the scanner
      }
+  }
+
+  private static Token checkLongerLex(String fileContents, int i, int lineNo) {
+
+    if((i+1) < fileContents.length()){
+
+      if(fileContents.startsWith("!=", i)){
+
+        return new Token(TokenType.BANG_EQUAL, "!=", null, lineNo);
+
+      } else if (fileContents.startsWith("<=", i)){
+
+        return new Token(TokenType.LESS_EQUAL, "!=", null, lineNo);
+
+      }
+      else if (fileContents.startsWith(">=", i)){
+
+        return new Token(TokenType.GREATER_EQUAL, "!=", null, lineNo);
+
+      }
+      else if (fileContents.startsWith("==", i)){
+
+        return new Token(TokenType.EQUAL_EQUAL, "!=", null, lineNo);
+
+      }
+    }
+    return null;
   }
 
   private static void grammarInit() {
@@ -115,6 +157,8 @@ public class Main {
     lexGrammar.put('=', TokenType.EQUAL);
     lexGrammar.put('>', TokenType.GREATER);
     lexGrammar.put('<', TokenType.LESS);
+
+    lexGrammar.put('"', TokenType.STRING);
 
 
   }
